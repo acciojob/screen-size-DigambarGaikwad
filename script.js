718585402d1fdf7ba94b11c4257f9f0eaf8e47a6
DigambarGaikwad <104820408+DigambarGaikwad@users.noreply.github.com>
2023-05-24 14:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let sizeElement=document.createElement("h1");
 sizeElement.innerText=`width: ${window.innerWidth} and height:${window.innerHeight}`
 document.body.append(sizeElement);
 
-windows.onresize=function() {
-	sizeElement.innerText=`width: ${windows.innerWidth} and height:${windows.innerHeight}`
-    documet.body.append(sizeElement);
-}
+// windows.onresize=function() {
+// 	sizeElement.innerText=`width: ${windows.innerWidth} and height:${windows.innerHeight}`
+//     documet.body.append(sizeElement);
+// }
+
+window.onresize = function() {
+      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+      console.log("Window width: " + width + "px");
+      console.log("Window height: " + height + "px");
