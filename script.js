fe965624ea4583e71ec513aa603f67c9dd136abf
DigambarGaikwad <104820408+DigambarGaikwad@users.noreply.github.com>
2023-05-24 14:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let sizeElement=document.createElement("h1");
+sizeElement.innerText=`width: ${window.innerWidth} and height:${window.innerHeight}`
+document.body.append(sizeElement);
+
+windows.onresize=function() {
+	sizeElement.innerText=`width: ${windows.innerWidth} and height:${windows.innerHeight}`
+    documet.body.append(sizeElement);
+}
